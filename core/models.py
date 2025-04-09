a79697fc6b146cab46f51ccc7270a53d291f9d98
Vivekroy286 <286royv@gmail.com>
2025-04-09 12:36:18 +0530

    created models

--- core/models.py
+++ core/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Question(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    text = models.TimeField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return self.text[:50]
+    
+    
+class Answer(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE, related_name='answers')
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    text = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    likes = models.ManyToManyField(User, related_name='liked_answers', blank=True)
+    
+    def __str__(self):
+        return self.text[:50]
+    
+    def total_likes(self):
+        return self.likes.count()
+    
+    
+    
+
+
