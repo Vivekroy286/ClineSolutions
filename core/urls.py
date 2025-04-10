ea7def68f66e89e15c2c6aed7560ed559b10cbb2
Vivekroy286 <286royv@gmail.com>
2025-04-10 20:58:59 +0530

    layout

--- core/urls.py
+++ core/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home_view, name='Homepage'),
+    path('', views.home_view, name='home'),
     path('question/<int:question_id>/',views.question_detail_view, name='question_detail'),
     path('answer/<int:answer_id>/like/', views.like_answer_view, name='like_answer'),
 ]
